2033ed838d0aa5b80d8896dd1c6971a367db6823
trevjnels <trevorjohnnels@gmail.com>
2019-03-28 18:10:56 -0600

    got some of the weather running, not to have the fam test it

--- index.js
+++ index.js
@@ -13,18 +13,44 @@ const copyrightListner = function() {
 };
 const ipKey = "316961512d48d2f24ad46bfd87a7bc6f";
 const weatherKey = "eb0f6c8bad479b1a1f57b441165e3edc";
+const kelvin2F = function(k) {
+  var f = k * (9 / 5) - 459.67;
+  return f;
+};
 
-const renderCurrentWeather = function() {};
+const renderCurrentWeather = function(resp) {
+  var temp = kelvin2F(resp.main.temp)
+    .toString()
+    .slice(0, 4);
+  var maxTemp = kelvin2F(resp.main.temp_max)
+    .toString()
+    .slice(0, 4);
+  var minTemp = kelvin2F(resp.main.temp_min)
+    .toString()
+    .slice(0, 4);
+  var clouds = resp.clouds.all;
+  var weather = [];
+  resp.weather.forEach(function(elem) {
+    weather.push(elem);
+  });
+
+  $(".output-left").html(`<div class="flex current-weather">
+    <H1>Today's Weather:</H1>
+    <p>Current Temp: ${temp}</p>
+    <p>High: ${maxTemp}</p>
+    <p>Low: Temp: ${minTemp}</p>
+  </div>`);
+};
 const renderFutureWeather = function() {};
 const renderCity = function(city) {
   $(".title").html(`Weather in ${city}:`);
 };
 
-const render = function(response) {
-  renderCity(response.city);
+const render = function(ipLocation) {
+  renderCity(ipLocation.city);
 
-  var lat = response.latitude;
-  var long = response.longitude;
+  var lat = ipLocation.latitude;
+  var long = ipLocation.longitude;
   getCurrentWeather(lat, long);
   getFutureWeather(lat, long);
 };
@@ -37,12 +63,20 @@ const getIPAddress = function() {
 };
 
 const getCurrentWeather = function(lat, long) {
-  fetch(`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}`)
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${weatherKey}`
+  )
     .then(response => response.json())
-    .then(responseJSON => console.log(responseJSON));
+    .then(responseJSON => renderCurrentWeather(responseJSON));
 };
 
-const getFutureWeather = function(lat, long) {};
+const getFutureWeather = function(lat, long) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&APPID=${weatherKey}`
+  )
+    .then(response => response.json())
+    .then(responseJSON => responseJSON);
+};
 
 const autoRunner = function() {
   logger(autoRunner);
